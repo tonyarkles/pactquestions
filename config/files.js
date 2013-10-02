f7abda60015cf87d0d8fcb55b8b3133ad3d71672
Tony Arkles <tony@peakxp.com>
2013-10-02 15:04:39 -0600

    adding vendor stuffs

--- config/files.js
+++ config/files.js
@@ -8,10 +8,31 @@
  *  regardless of the point you're at in the build
  *  lifecycle.
  *
- * To see the default definitions for all of Lineman's file paths and globs, look at:
- * https://github.com/testdouble/lineman/blob/master/config/files.coffee
+ * You can find the parent object in: node_modules/lineman/config/files.coffee
  */
 
 module.exports = require(process.env['LINEMAN_MAIN']).config.extend('files', {
-  //Override file patterns here
+  js: {
+    vendor: [
+      "vendor/js/jquery.js",
+      "vendor/js/underscore.js",
+      "vendor/js/backbone.js",
+      "vendor/js/**/*.js"
+    ],
+    app: [
+      "app/js/app.js",
+      "app/js/models/**/*.js",
+      "app/js/services/**/*.js",
+      "app/js/views/**/*.js",
+      "app/js/router.js"
+    ]
+  },
+
+  less: {
+    compile: {
+      options: {
+        paths: ["vendor/css/normalize.css", "vendor/css/**/*.css", "app/css/**/*.less"]
+      }
+    }
+  }
 });
